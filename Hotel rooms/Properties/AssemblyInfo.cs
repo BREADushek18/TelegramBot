97e569e9ec5562c9686d69f75292accef4c6ca0e
Dima <117664428+BREADushek18@users.noreply.github.com>
2023-06-05 01:49:34 +0700

    Update AssemblyInfo.cs

--- Hotel rooms/Properties/AssemblyInfo.cs
+++ Hotel rooms/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // Общие сведения об этой сборке предоставляются следующим набором
 // набора атрибутов. Измените значения этих атрибутов для изменения сведений,
 // связанные с этой сборкой.
-[assembly: AssemblyTitle("HotelReservation")]
+[assembly: AssemblyTitle("DnDrandomiser")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("HotelReservation")]
+[assembly: AssemblyProduct("DnDrandomiser")]
 [assembly: AssemblyCopyright("Copyright ©  2023")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
